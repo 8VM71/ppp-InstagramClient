b3eb5c62816a09b388632e08413823f06e6bb450
SergeiChernykh <sergey7@tpu.ru>
2018-10-02 17:35:36 +0700

    * fixed getUserPhotos method

--- sources/SearchPage.qml
+++ sources/SearchPage.qml
@@ -1,9 +1,49 @@
 import QtQuick 2.0
 
+import "service.js" as Service
+
 Item {
     property string title: qsTr("Поиск")
 
     signal next
     signal back
+
+
+    Component.onCompleted: {
+        var service = new Service.Service()
+        service.setToken(appSettings.vkToken)
+//        service.searchUsers("inst", function(reply) {
+//            console.debug("Success:", reply.success)
+//            if(reply.success) {
+//                console.debug("Data:", JSON.stringify(reply.data))
+//            } else {
+//                console.debug(reply.errorString)
+//            }
+//        })
+//        service.getUserInfo("117812625", function(reply) {
+//            console.debug("Success:", reply.success)
+//            if(reply.success) {
+//                console.debug("User Data:", JSON.stringify(reply.data))
+//            } else {
+//                console.debug(reply.errorString)
+//            }
+//        })
+//        service.getAppPermissions("117812625", function(reply) {
+//            console.debug("Success:", reply.success)
+//            if(reply.success) {
+//                console.debug("App permissions:", JSON.stringify(reply.data))
+//            } else {
+//                console.debug(reply.errorString)
+//            }
+//        })
+        service.getUserPhotos("117812625", function(reply) {
+            console.debug("Success:", reply.success)
+            if(reply.success) {
+                console.debug("User Photos:", JSON.stringify(reply.data))
+            } else {
+                console.debug(reply.errorString)
+            }
+        })
+    }
 
 }
--- sources/service.js
+++ sources/service.js
@@ -1,6 +1,6 @@
 .import "utils.js" as Utils
 
-var token = "65864d4e5c038b45da86a38e1f00b4b245095dbad3f1637d3ffae8c3bf35be6633be403820126dcae4294";
+//var token = "65864d4e5c038b45da86a38e1f00b4b245095dbad3f1637d3ffae8c3bf35be6633be403820126dcae4294";
 
 function Service() {
 
@@ -41,7 +41,7 @@ Service.prototype.getAppPermissions = function(userId, callback) {
     var queryParams = {
         user_id : userId,
         v : "5.8",
-        access_token : token
+        access_token : this.token
     };
 
     url = Utils.combineUrl(url, queryParams);
@@ -55,7 +55,7 @@ Service.prototype.searchUsers = function(searchQuery, callback) {
         q : searchQuery,
         v : "5.8",
         fields : "photo_big,screen_name,city,followers_count",
-        access_token : token
+        access_token : this.token
     };
 
     url = Utils.combineUrl(url, queryParams);
@@ -69,7 +69,7 @@ Service.prototype.getUserInfo = function(userId, callback) {
         user_ids : userId,
         v : "5.8",
         fields : "photo_id,verified,sex,bdate,city,country,home_town,has_photo,photo_50,photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,followers_count,common_count,occupation,nickname,relatives,relation,personal,connections,exports,activities,interests,music,movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,can_write_private_message,can_send_friend_request,is_favorite,is_hidden_from_feed,timezone,screen_name,maiden_name,crop_photo,is_friend,friend_status,career,military,blacklisted,blacklisted_by_me",
-        access_token : token
+        access_token : this.token
     };
 
     url = Utils.combineUrl(url, queryParams);
@@ -78,13 +78,13 @@ Service.prototype.getUserInfo = function(userId, callback) {
 }
 
 Service.prototype.getUserPhotos = function(userId, callback) {
-    var url = "https://api.vk.com/method/photos.getUserPhotos";
+    var url = "https://api.vk.com/method/photos.getAll";
     var queryParams = {
-        user_id : userId,
-        count: "1000",
+        owner_id : userId,
+        count: "100",
         extended: "1",
         v : "5.8",
-        access_token : token
+        access_token : this.token
     };
 
     url = Utils.combineUrl(url, queryParams);
